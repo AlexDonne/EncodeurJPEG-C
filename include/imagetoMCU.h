b0813fdba880475a94f4677c116630eed7d9ffbe
Alexandre Donné <alexandre.donne@grenoble-inp.org>
2018-05-15 17:08:17 +0200

    Structures communes

--- include/imagetoMCU.h
+++ include/imagetoMCU.h
@@ -1,10 +1,6 @@
 #ifndef IMAGE_TO_MCU_H
 #define IMAGE_TO_MCU_H
 
-typedef struct pixelRGB PixelRGB;
-
-typedef struct pixelNB PixelNB;
-
 typedef struct imagePPM ImagePPM;
 
 typedef enum TYPE_IMAGE TYPE_IMAGE;
---
+++ include/structures.h
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#ifndef ETU_STRUCTURES_H
+#define ETU_STRUCTURES_H
+
+typedef struct pixelRGB{
+        int rouge;
+        int bleu;
+        int vert;
+}PixelRGB;
+
+typedef struct pixelNB{
+    int valeur;
+}PixelNB;
+
+typedef struct mcupixels{
+    PixelRGB **blocsRGB;
+    PixelNB **blocsNB;
+}MCUPixels;
+
+typedef struct mcuTransform{
+    uint8_t Y[8][8];
+    int8_t Cb[8][8];
+    int8_t Cr[8][8];
+
+}MCUTransform;
+
+#endif //ETU_STRUCTURES_H
