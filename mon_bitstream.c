d8fcc8922953ab44a99d8077365869f349c2d4a0
Sébastien Herbreteau <sebastien.herbreteau@grenoble-inp.org>
2018-05-22 10:57:12 +0200

    mon_bitstream.c gère le byte stuffling : à tester

--- mon_bitstream.c
+++ mon_bitstream.c
@@ -25,25 +25,41 @@ struct bitstream *bitstream_create(const char *filename) {
     2.10.4 du sujet). Attention nbits doit être compris entre 1 et 32.
 */
 void bitstream_write_nbits(struct bitstream *stream, uint32_t value, uint8_t nbits, bool is_marker) {
-        if (nbits + stream->indice <= 8) {
-          stream->buffer += (value << (32 - nbits)) >> (24 + stream->indice);
-          stream->indice += nbits;
-        }
-        else {
-          /* on écrit la première partie de value */
-          uint8_t value1 = (value << (32 - nbits)) >> (24 + stream->indice);
-          value1 += stream->buffer;
-          fwrite(&value1, sizeof(uint8_t), 1, stream->fichier);
-
-          /* on efface les bits au delà de nbits */
-          nbits -= 8 - stream->indice;
-          uint32_t value2 = (value << (32 - nbits)) >> (32 - nbits);
-
-          /* on écrit la seconde partie de value */
+        if (is_marker) {
+          bitstream_flush(stream);
           stream->buffer = 0;
           stream->indice = 0;
-          bitstream_write_nbits(stream, value2, nbits, is_marker);
+          fwrite(&value, sizeof(uint8_t), 1, stream->fichier);
         }
+        else {
+          if (nbits + stream->indice <= 8) {
+            stream->buffer += (value << (32 - nbits)) >> (24 + stream->indice);
+            stream->indice += nbits;
+          }
+          else {
+            /* on écrit la première partie de value */
+            uint8_t value1 = (value << (32 - nbits)) >> (24 + stream->indice);
+            value1 += stream->buffer;
+            fwrite(&value1, sizeof(uint8_t), 1, stream->fichier);
+
+            /* gestion du byte stuffing */
+            if (value1 == 0xff) {
+              value1 = 0;
+              fwrite(&value1, sizeof(uint8_t), 1, stream->fichier);
+            }
+
+            /* on efface les bits au delà de nbits */
+            nbits -= 8 - stream->indice;
+            uint32_t value2 = (value << (32 - nbits)) >> (32 - nbits);
+
+            /* on écrit la seconde partie de value */
+            stream->buffer = 0;
+            stream->indice = 0;
+            bitstream_write_nbits(stream, value2, nbits, is_marker);
+          }
+        }
+
+
 };
 
 /*
